cce0d8f55690242ca301e3064837e5acfcb600fe
dwinandar <adedwinandar50@gmail.com>
2023-11-02 22:02:27 +0700

    fix: error page unknown type

--- src/error-page.tsx
+++ src/error-page.tsx
@@ -2,14 +2,17 @@ import { useRouteError } from "react-router-dom";
 
 export default function ErrorPage() {
   const error = useRouteError();
+
+  const errorMessage = error instanceof Error ? error.message : "An unknown error occurred";
+
   console.error(error);
 
   return (
-    <div id="error-page">
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
+    <div id="error-page" className="flex flex-col items-center justify-center min-h-screen">
+      <h1 className="text-3xl">Oops!</h1>
+      <p className="text-xl">Sorry, an unexpected error has occurred.</p>
       <p>
-        <i>{error.statusText || error.message}</i>
+        <i>{errorMessage}</i>
       </p>
     </div>
   );
